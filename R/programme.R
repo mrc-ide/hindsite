12c17ffa0b17fdc224acce6662c7e02c1f7d6476
Peter Winskill <p.winskill@imperial.ac.uk>
2022-09-22 09:07:08 +0100

    par_pf par_pv bug fix

--- R/programme.R
+++ R/programme.R
@@ -21,7 +21,7 @@ add_case_detection_cost <- function(x){
       pf_api = ifelse(.data$par_pf > 0, .data$cases_pf / .data$par_pf * 1000, 0),
       pv_api = ifelse(.data$par_pv > 0, .data$cases_pv / .data$par_pv * 1000, 0),
       cost_proactive_case_detection =
-                    ifelse(pf_api < 1 & pv_api < 1,
+                    ifelse(.data$pf_api < 1 & .data$pv_api < 1,
                            treasure::cost_pacd(n_tested = .data$cases_pf + .data$cases_pv),
                            0)) |>
     dplyr::select(-.data$pf_api, -.data$pv_api)
