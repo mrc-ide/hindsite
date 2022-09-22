55d2a5ecaa70d56e6fe6feb700e3b5d75263d17f
Peter Winskill <p.winskill@imperial.ac.uk>
2022-09-22 09:03:58 +0100

    Divide by 0 PCD fix

--- R/programme.R
+++ R/programme.R
@@ -17,8 +17,12 @@ add_surveillance_cost <- function(x){
 #' @export
 add_case_detection_cost <- function(x){
   x |>
-    dplyr::mutate(cost_proactive_case_detection =
-                    ifelse(.data$cases_pf / .data$par_pf * 1000 < 1 & .data$cases_pv / .data$par_pv * 1000 < 1,
+    dplyr::mutate(
+      pf_api = ifelse(.data$par_pf > 0, .data$cases_pf / .data$par_pf * 1000, 0),
+      pv_api = ifelse(.data$par_pv > 0, .data$cases_pv / .data$par_pv * 1000, 0),
+      cost_proactive_case_detection =
+                    ifelse(pf_api < 1 & pv_api < 1,
                            treasure::cost_pacd(n_tested = .data$cases_pf + .data$cases_pv),
-                           0))
+                           0)) |>
+    dplyr::select(-.data$pf_api, -.data$pv_api)
 }
